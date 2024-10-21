e5143afb48536265944b448747111ec8a599300c
Grace <graciacr2004@gmail.com>
2024-10-21 12:44:05 +0200

    Modificación de index.php para recorrer la carpeta profile

--- index.php
+++ index.php
@@ -1,22 +1,58 @@
-<body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
-        <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de Alumnos</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
         }
-    ?>
-    </il>
+        td {
+            border: 1px solid black;
+            padding: 10px;
+            text-align: center;
+        }
+        img {
+            width: 100px;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Projecte Welcome 1</h1>
+    </header>
+    <main>
+        <section>
+            <table>
+                <tr>
+                    <?php
+                    $htmls = scandir("./profile", SCANDIR_SORT_ASCENDING);
+                    $count = 0;
+                    foreach( $htmls as $html ) {
+                        if( $html == "." || $html == ".." ) continue;
+                        if( substr($html, -5) == ".html" ) {
+                            if($count % 10 == 0 && $count != 0) {
+                                echo "</tr><tr>";
+                            }
+                            $name = substr($html, 0, -5);
+                            echo "<td><a href='profile/$html'>$name</a></td>";
+                            $count++;
+                        }
+                    }
+                    while ($count % 10 != 0) {
+                        echo "<td></td>";
+                        $count++;
+                    }
+                    ?>
+                </tr>
+            </table>
+        </section>
+    </main>
+    <footer>
+        <p>Proyecto Welcome clemente-elBourkhissi</p>
+    </footer>
 </body>
+</html>
